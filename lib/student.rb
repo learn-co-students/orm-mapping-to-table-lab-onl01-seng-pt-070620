636bf1a6bad96c578d3f36b5700c531d13626247
Lauren Anderson <laurenaanderson95@gmail.com>
2020-09-21 17:26:00 -0400

    starting point

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,28 @@
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
 
+  def initialize(id: nil, name:, grade:)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+
+  end
+
+  def self.drop_table
+
+  end
+
+  def save
+
+  end
+
+  def self.create
+    
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
